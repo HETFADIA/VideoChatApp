2780dd8d76f78f0200d0288371f18157612a2f5f
Het <62541263+HETFADIA@users.noreply.github.com>
2021-06-25 02:06:16 +0530

    updated

---
+++ public/script.js
@@ -0,0 +1,16 @@
+const myVideo = document.createElement('video')
+myVideo.muted=true
+let myVideoStream
+navigator.mediaDevices.getUserMedia({
+    video:true,
+    audio:true
+}).then(stream=>{
+    myVideoStream=stream;
+})
+
+const addVideoStream=(video,stream)=>{
+    video.srcObject=stream;
+    video.addEventListener('loadmetadata',()=>{
+        video.play();
+    })
+}
