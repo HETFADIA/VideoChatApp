b8c1f5a21c89f9a1658b81dfa91674396dd0de4f
Het <62541263+HETFADIA@users.noreply.github.com>
2021-07-01 02:46:42 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,17 +1,12 @@
 const express = require('express')
 const app = express()
-// const cors = require('cors')
-// app.use(cors())
 const server = require('http').Server(app)
 const io = require('socket.io')(server)
 const { ExpressPeerServer } = require('peer');
-const peerServer = ExpressPeerServer(server, {
-  debug: true
-});
+const peerServer = ExpressPeerServer(server, {debug: true});
 const { v4: uuidV4 } = require('uuid')
 
 app.use('/peerjs', peerServer);
-
 app.set('view engine', 'ejs')
 app.use(express.static('public'))
 
@@ -23,20 +18,22 @@ app.get('/:room', (req, res) => {
   res.render('room', { roomId: req.params.room })
 })
 
-io.on('connection', socket => {
-  socket.on('join-room', (roomId, userId) => {
-    socket.join(roomId)
-    socket.to(roomId).emit('user-connected', userId);
-    
-    socket.on('message', (message) => {
-      
-      io.to(roomId).emit('createMessage', message)
-  }); 
+var users=[];
 
-    socket.on('disconnect', () => {
-      socket.to(roomId).emit('user-disconnected', userId)
+io.on('connection', socket => {
+    socket.on('join-room', (roomId, userId) => {
+        socket.join(roomId)
+        socket.to(roomId).emit('user-connected', userId);
+        
+        socket.on('message', (message) => {
+          
+          io.to(roomId).emit('createMessage', message)
+    }); 
+
+        socket.on('disconnect', () => {
+          socket.to(roomId).emit('user-disconnected', userId)
+        })
     })
-  })
 })
 
 server.listen(process.env.PORT||3030)
