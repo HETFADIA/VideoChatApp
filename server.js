eab129191bb040190dfd7d392aa0f5b1817e4baf
Het <62541263+HETFADIA@users.noreply.github.com>
2021-06-30 16:14:17 +0530

    update

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ app.get('/:room', (req, res) => {
 io.on('connection', socket => {
   socket.on('join-room', (roomId, userId) => {
     socket.join(roomId)
-    socket.to(roomId).broadcast.emit('user-connected', userId);
+    socket.to(roomId).emit('user-connected', userId);
     // messages
     socket.on('message', (message) => {
       //send message to the same room
@@ -34,7 +34,7 @@ io.on('connection', socket => {
   }); 
 
     socket.on('disconnect', () => {
-      socket.to(roomId).broadcast.emit('user-disconnected', userId)
+      socket.to(roomId).emit('user-disconnected', userId)
     })
   })
 })
